2d0f7609df597c49f16c5ac1e609eaafd5170dd9
winrout <el18187@mail.ntua.gr>
2024-04-28 09:28:17 +0300

    implement user-data microservice

---
+++ service-user-data/README.md
@@ -0,0 +1,16 @@
+# MICROSERVICE
+
+## User Data Service
+In this service is hosted the server.js file, which handles the mongo database of the users. Packages used:
+- kafkajs
+- mongoose
+
+Install the node packages first:
+```sh
+npm install
+```
+
+To start the service:
+```sh
+npm run start
+```
---
+++ service-user-data/server.js
@@ -0,0 +1,82 @@
+const { Kafka } = require("kafkajs")
+const mongoose = require("mongoose")
+
+const kafka = new Kafka({
+    clientId: "service-user-data",
+    brokers: ["localhost:29092"],
+    retries: 10,
+});
+
+const producer = kafka.producer();
+const consumer = kafka.consumer({ groupId: "service-user-data" });
+
+
+const main = async () => {
+
+    await mongoose.connect("mongodb://user:pass@localhost:27017/Users?authSource=admin");
+
+    const userSchema = new mongoose.Schema({
+        email: String,
+        name: String,
+        credits: Number
+    })
+
+    const User = mongoose.model("User", userSchema)
+
+    await producer.connect()
+    await consumer.connect()
+
+    await consumer.subscribe({
+        topics: [
+            "user-create-request",
+            "user-credits-update-request",
+            "user-get-request",
+        ],
+        fromBeginning: false
+    })
+
+    await consumer.run({
+        eachMessage: async ({ message, topic }) => {
+
+            if (topic === "user-create-request") {    
+                const email = message.key.toString();
+                const name = message.value.toString();
+
+                const newUser = new User({
+                    email: email,
+                    name: name,
+                    credits: 0
+                });
+                
+                await newUser.save();
+            }
+
+            else if (topic === "user-credits-update-request") {
+                const email = message.key.toString();
+                const added_credits = Number(message.value.toString());
+
+                await User.findOneAndUpdate(
+                    { email: email},
+                    { $inc: { credits: added_credits }}
+                );
+            }
+
+            else if (topic == "user-get-request") {
+                const email = message.value.toString();
+                const user = await User.findOne({ email: email });
+
+                producer.send({
+                    topic: "user-get-response",
+                    messages: [
+                        { key: email, value: JSON.stringify(user) }
+                    ]
+                });
+            }
+        }
+    });
+    console.log("Service user-data is running");
+
+};
+
+
+main();
