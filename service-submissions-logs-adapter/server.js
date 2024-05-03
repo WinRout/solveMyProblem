0f47038be8ee4e7fca77898c7079f82d687b515c
winrout <el18187@mail.ntua.gr>
2024-05-03 03:01:01 +0300

    new logs services

---
+++ service-submissions-logs-adapter/server.js
@@ -0,0 +1,80 @@
+const { Kafka } = require("kafkajs");
+const express = require("express");
+
+const app = express();
+
+const kafka = new Kafka({
+    clientId: "service-submissions-logs-adapter",
+    brokers: ["localhost:29092"],
+    retries: 10,
+});
+
+const producer = kafka.producer();
+const consumer = kafka.consumer({ groupId: "service-submissions-logs-adapter" });
+
+// a variable for storing responses from kafka
+let logs = undefined
+
+const main = async () => {
+    await producer.connect();
+    await consumer.connect();
+
+    await consumer.subscribe({
+        topics: [
+            "submissions-logs-get-response"
+        ],
+        fromBeginning: true
+    });
+    
+    // kafka messages handling
+    await consumer.run({
+        eachMessage: async ({ topic, message }) => {
+            if (topic === "submissions-logs-get-response") {
+                logs = JSON.parse(message.value.toString());
+            }
+        }
+    });
+
+    // express app (api for frontend) handling
+    app.use((req, res, next) => {
+        // for CORS shake... (:
+		res.set({
+			"Access-Control-Allow-Origin": "*",
+			"Access-Control-Allow-Headers": "Content-Type"
+		});
+
+		next();
+	}, express.json());
+
+    app.get('/submissions-statistics-get/:type', async (req, res) => {
+
+        producer.send({
+            topic: "submissions-logs-get-request",
+            messages: [
+                { value: req.params.type }
+            ]
+        });
+        /*
+        - A Promise represents the eventual outcome (resolve, reject) of an async operation.
+        - In this case we want to asynchroniously wait to receive message in kafka topic,
+        so we "resolve" the promise after 100ms.
+        - When received logs from kafka topic, I can continue
+        */
+        while (logs === undefined) {
+            await new Promise(resolve => setTimeout(resolve, 500));
+        }
+
+        res.send(logs);
+
+        // Forget user, so next time will be surely updated before res.send
+        logs = undefined;
+    });
+
+
+
+    app.listen(4012, () => {
+		console.log("Service submissions-logs-adapter is running");
+	});
+}
+
+main();
---
+++ service-submissions-logs-data/server.js
@@ -0,0 +1,83 @@
+const { Kafka } = require("kafkajs")
+const mongoose = require("mongoose")
+
+const kafka = new Kafka({
+    clientId: "service-submissions-logs-data",
+    brokers: ["localhost:29092"],
+    retries: 10,
+});
+
+const producer = kafka.producer();
+const consumer = kafka.consumer({ groupId: "service-submissions-logs-data" });
+
+
+const main = async () => {
+
+    await mongoose.connect("mongodb://user:pass@localhost:27018/Logs?authSource=admin");
+
+    const LogSchema = new mongoose.Schema({
+        email: String,
+        solver_type: String,
+        execution_date: Date,
+        execution_secs: Number,
+    })
+
+    const Log = mongoose.model("Log", LogSchema)
+
+    await producer.connect()
+    await consumer.connect()
+
+    await consumer.subscribe({
+        topics: [
+            "solvers-general-response",
+            "submissions-logs-get-request"
+        ],
+        fromBeginning: false
+    })
+
+    await consumer.run({
+        eachMessage: async ({ message, topic }) => {
+
+            if (topic == "solvers-general-response") {
+                const data = JSON.parse(message.value.toString());
+                
+                // Create log
+                const newLog = new Log({
+                    email: data.email,
+                    solver_type: data.solver_type,
+                    execution_date: data.execution_date,
+                    execution_secs: data.execution_secs
+                });
+                
+                await newLog.save();
+
+                // Also notify user-data service to decrease credits by execution time
+                const spent_credits = -1 * Math.abs(data.execution_secs);
+                
+                producer.send({
+                    topic: "user-credits-update-request",
+                    messages: [ 
+                        { key: data.email, value: spent_credits.toString()}
+                    ]
+                });
+            }
+            else if (topic == "submissions-logs-get-request") {
+                const solver_type = message.value.toString()
+
+                const logs = await Log.find({ solver_type: solver_type });
+
+                producer.send({
+                    topic: "submissions-logs-get-response",
+                    messages: [ 
+                        { value: JSON.stringify(logs) }
+                    ]
+                });
+            }
+        }
+    });
+    console.log("Service submissions-logs-data is running");
+
+};
+
+
+main();
